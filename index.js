1d57c38c260ac3930ef6ea47e0515a0f777aa404
Nexproc <ntitcombe@gmail.com>
2015-09-08 09:24:20 -0700

    added username param

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 var users = 0;
-var pastMessages = [];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  io.emit('new user', 'User #' + (++users));
-  var username = "User " + users;
+io.on('connection', function(socket, options){
+  var username = "";
+  if (options.username) {
+    io.emit('new user', options.username);
+    username = options.username;
+  } else {
+    io.emit('new user', 'User #' + (++users));
+    username = "User " + users;
+  }
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
     var message = username + ": " + msg;
