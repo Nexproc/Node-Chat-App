0ad30e4d393ae2f9a052a616c0d02deb297b73ff
Nexproc <ntitcombe@gmail.com>
2015-09-09 21:24:41 -0700

    trying everything

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ io.on('connection', function(socket) {
   socket.room = "-1";
   socket.join(socket.room);
 
-  // get user inf
+  // get user info
   socket.emit('user info request');
   socket.on('user info response', function (uname, team_id) {
     socket.username = uname;
@@ -23,14 +23,14 @@ io.on('connection', function(socket) {
       socket.join(socket.room);
     }
     socket.broadcast.in(socket.room).emit('new user', socket.username);
-    socket.emit('update room', socket.room);
+    // socket.emit('update room', socket.room);
   });
 
   socket.on('switch team', function (team_id) {
     socket.leave(socket.room);
     socket.room = team_id;
     socket.join(socket.room);
-    socket.emit('update room', socket.room);
+    // socket.emit('update room', socket.room);
   });
 
   socket.on('chat message', function (msg) {
